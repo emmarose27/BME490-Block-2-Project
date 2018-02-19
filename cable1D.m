7d5d008cfad938ab8c1faea412b01704e977503b
bheater <bheater88@gmail.com>
2018-02-19 15:20:06 -0600

    first comment of notes from class as comments to guide the model

---
+++ cable1D.m
@@ -0,0 +1,14 @@
+%% 1D Cable Ionic Model
+
+% Guess the space step
+% Solve the equation
+% look at the propogatio velocity in the middle of the cable
+
+% Set any length to the fiber
+% Find:
+% 1. timestep
+% 2. space step
+
+% Spacially
+% Find wavelength: propogation velocity * time
+% Make model 4*wavelength
