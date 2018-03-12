806d6cec9f0a94731ff90c4a2b9013d45a8c7648
bheater <bheater88@gmail.com>
2018-03-12 17:34:46 -0500

    Working 1D cable model, but need to ensure convergence by assessing propogation velocity in the fiber and adjusting the space-step before we move on the 2D model.

--- cable1D.m
+++ cable1D.m
@@ -1,18 +1,3 @@
-%% 1D Cable Ionic Model
-
-% Guess the space step
-% Solve the equation
-% look at the propogation velocity in the middle of the cable
-
-% Set any length to the fiber
-% Find:
-% 1. timestep
-% 2. space step
-
-% Spacially
-% Find wavelength: propogation velocity * time
-% Make model 4*wavelength
-
 %% FitzHugh Nagumo system 
 %
 % The equations for the FHN system are 
@@ -51,80 +36,61 @@
 % physiologic values for CV and APD. Reasonable values are transverse 
 % conduction velocity (CV) ~= 0.2 m/sec and APD ~=200 ms.
 
-% The equations for the FHN system are 
-% 
-% du/dt = D1*d2u/dx2 + D2*d2u/dy2 + c1*u(u-a)*(1-u) - c2*u*v
-% dv/dt = b*(u-v)
-% 
-% u is the "membrane potential". 
-% The resting potential is 0 and depolarized potential < 1. 
-% v is a "recovery variable" & doesn't have a direct physiologic equivalent. 
-% D1 is the diffusion constant (i.e., conductivity) in the x direction, and
-% D2 is diffusion in the y direction.
-% If you want isotropic tissue, make them both 1. 
-% If you want propagation faster in one direction (fiber direction),
-% make one of them ~4 times bigger.
+%% Convergence Notes from Pollard
 
-fiber_length = 2/100; %fiber length in meters
-dx = fiber_length/100; %space step in meters
-v0 = 0.0; u0 = 0.0;% resting potential 
-% boundary conditions
-% d2v0_dx2 = (v(1)-v0)/(dx*dx);
+% You want to fix the length of your cable (so that when dx is halved, you
+% have twice as many nodes). Looking at propagation speed (e.g., time to
+% propagate across a portion of the cable...like the middle third) as a
+% function of dx is a good way to tell if the model is converged spatially.
+% When dx is too big, there won't be enough nodes across the wavefront,
+% which will alter the shape of the wavefront and change propagation speed.
+% When dx converges, speed will stabilize.
+
+%% 1D Cable Equation
+nodes = 100;dx = 1;
+d2udx2 = zeros(1,nodes);
+x0 = d2udx2;
+v0 = 0.25; v1 = 0;
+x0(1) = v0; 
+% boundary condition: special diffusive term for 1st and last node
+d2udx2(1) = (v1-v0)/(dx*dx);
+d2udx2(nodes) = d2udx2(1);
 
 % isotropic tissue: D1 and D2 = 1
 D1 = 1; D2 = 1;
 % constants
 a=0.13; b=0.013; c1=0.26; c2=0.1;
-% Solve 1st order ODE
-t = [1 100];
-%v(1,1) = zeros(length(t));% resting potential 
+% Solve 1st order ODE: https://www3.nd.edu/~nancy/Math20750/Demos/3dplots/dim3system.html
+% dudt = x(1)
+%dudt = D1 *d2udx2(1) +  c1*u(u-a)*(1-u) - c2*u*v;
+%dxdt = (D1 * d2udx2(1)) + ( c1*x(1)*(x(1)-a)*(1 - x(1)))- (c2*x(1)*x(2));
+% dudt = D1*d2udx2 + D2*d2udy2 + c1*u(u-a)*(1-u) - c2*u*v;
+%dvdt = x(2)
+%dvdt = b*(u-v);
+%dydt = b*(x(1)-x(2));
+f = @(t,x) [b*(x(1)-x(2)); (D1*d2udx2(1))+(c1*x(1)*(x(1)-a)*(1-x(1)))-(c2*x(1)*x(2));];
+tspan = [0 10];
+[t, x] = ode45(f,tspan,[0.25, 0]);
+dt = diff(t);
+%hold on; plot(t,xa(:,2));  legend('Vm(t)','du/dt')
+plot(t,x(:,1)); hold off;
+title('V(t)')
+xlabel('t'), ylabel('V')
+u = x(:,1);
+v = x(:,2);
+%Vm(1,:) = u;
 
-y = [u0,v0]; % initial conditions
-%[dudt, dvdt] = odefun(1,[u,v]);
-[t, y] = ode45(odefun,[1 100],0);
-% combine du/dt and dv/dt into one function that returns two values
-%from ode45 documentation: odefun would take in two arguments: the time and
-%the current values of u and v (as a vector) i.e. [t, [u, v]]
-%then it would return [dudt, dvdt]
-
-% odefun takes two arguments: t and y. t is the current time. 
-% y is a vector [u, v] which is the current value of u and v. 
-% it returns some other vector, let's call it z. 
-% z has the values for du/dt and dv/dt
-% dydt = odefun(t,y)
-
-% ode45 tries to reverse engineer a function from just its derivative. 
-% in this case, it's two functions from two derivatives. 
-% odefun is the function(s) used to calculate the derivatives at specific times and points
-
-%so we need a function that takes time, a value of u, and a value of v, and
-% returns the derivative of u(t) and derivative of v(t) at the points of u and v
-
-%so odefun takes t, [u, v] and returns [du/dt, dv/dt] b/c 
-function dydt = odefun(t,y)
-%     dudt = D1*d2u/dx2 + D2*d2u/dy2 + c1*u(u-a)*(1-u) - c2*u*v;
-%     dvdt = b*(u-v);
-    dydt = zeros(2,1);
-    dydt(1) = y(1)+2*y(2);
-    dydt(2) = 3*y(1)+2*y(2);
+% Propogate down the fiber
+% Finite difference analysis of inner nodes
+for n=2:nodes-1
+    d2udx2(n) = (x(n+1)+x(n-1)-2*x(n))/(dx*dx);
+    f = @(t,x) [b*(x(1)-x(2)); (D1*d2udx2(n))+(c1*x(1)*(x(1)-a)*(1-x(1)))-(c2*x(1)*x(2));];
+    [tn, xn] = ode45(f,t,[u(length(u)), v(length(v))]);
+    u = xn(:,1);
+    v = xn(:,2);
+    %Vm(n,length(xn)) = xn(:,1);
 end
+% plot 2nd deriv of V(t) over position
+position = 0:dx:nodes-1;
+figure;plot(position, d2udx2);title('d2u/dx2'); xlabel('position');
 
-% Spacial Derivative
-
-% boundary condition: special diffusive term for 1st and last node
-% d2vdx2_init0 = (v[2]-v[1])/(dx*dx);
-% d2vdx2 = (v(n+1)+v(n-1)-2*v(n))/(dx*dx);
-
-% guess spacestep
-%dx = 10.0; dy = 10.0;
-%[t,y] = ode45(odefun,[1,100],v0);
-
-% du/dt = D1*d2u/dx2 + D2*d2u/dy2 + c1*u(u-a)*(1-u) - c2*u*v
-% dv/dt = b*(u-v)
-%v0 = 0.0;
-% function d2vdx2 = odefun(t,y)
-%     d2vdx2 = (v(n+1)+v(n-1)-2*v(n))/(dx*dx);
-% end
-% function dvdt = odefun1(t,y)
-%     dvdt = (v(n+1)+v(n-1)-2*v(n))/(dx*dx);
-% end
